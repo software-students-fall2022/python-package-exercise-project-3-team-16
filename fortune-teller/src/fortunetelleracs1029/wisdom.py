82be25e004ce520fdba71d6e739e1c46837578b4
itSanjaya <113206972+itSanjaya@users.noreply.github.com>
2022-11-02 16:29:16 -0400

    Not reviewed code pushed, so deleted

--- fortune-teller/src/fortunetelleracs1029/wisdom.py
+++ fortune-teller/src/fortunetelleracs1029/wisdom.py
@@ -16,30 +16,7 @@ def get() :
     return random.choice(pieces_of_advice)
 
 
-def funny(name):
-    '''
-    Returns happy/funny quotes
-    '''
-    quotes = {
-        "Steven Wright": "I wish the first word I ever said was the word 'quote,' so right before I die I could say 'unquote.",
-        
-        "Issac Asimov": "Though sleep is called our best friend, it is a friend who often keeps us waiting!",
-        
-        "Leo Tolstoy": "Happiness is an allegory, unhappiness a story.",
-        
-        "J.K Rowling": "Happiness can be found even in the darkest of times; if only one remembers to turn on the light.",
-        
-        "Oscar Wilde": "Some cause happiness wherever they go; others, whenever they go.",
-        
-        "Mark Twain": "When your friends begin to flatter you on how young you look, it's a sure sign you're getting old.",
-    }
-    
-    if name in quotes.keys():
-        return random.choice(quotes.get(name))
-    elif name == "":
-        return random.choice(quotes.get(random.choice(list(quotes.keys()))))
-    else:
-        return "The author doesn't exist"
+
     
     
     
