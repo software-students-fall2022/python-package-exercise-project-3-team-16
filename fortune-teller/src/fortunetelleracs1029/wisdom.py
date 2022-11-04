69526c0d4deb0618c047b6f9c8ecd6c24cfcd473
Brandon Chen <bc2982@nyu.edu>
2022-11-04 02:43:50 -0400

    Returned an int for days rather than a float

--- fortune-teller/src/fortunetelleracs1029/wisdom.py
+++ fortune-teller/src/fortunetelleracs1029/wisdom.py
@@ -32,8 +32,8 @@ def getCSFortune(question, debug):
         if debug:
             return random.choice(possibleErrors)
         else:
-            days = random.random() * 100
-            return "You are on the right track and will finish your assignment/project in" + str(days)
+            days = int(random.random() * 100)
+            return "You are on the right track and will finish your assignment/project in " + str(days) + " days."
     else:
         return "You are screwed. Switch major. Go next."
 
@@ -88,22 +88,21 @@ def funny(name):
     '''
     quotes = {
         "Steven Wright": "I wish the first word I ever said was the word 'quote,' so right before I die I could say 'unquote.",
-        
+
         "Issac Asimov": "Though sleep is called our best friend, it is a friend who often keeps us waiting!",
-        
+
         "Leo Tolstoy": "Happiness is an allegory, unhappiness a story.",
-        
+
         "J.K Rowling": "Happiness can be found even in the darkest of times; if only one remembers to turn on the light.",
-        
+
         "Oscar Wilde": "Some cause happiness wherever they go; others, whenever they go.",
-        
+
         "Mark Twain": "When your friends begin to flatter you on how young you look, it's a sure sign you're getting old.",
     }
-    
+
     if name in quotes.keys():
         return random.choice(quotes.get(name))
     elif name == "":
         return random.choice(quotes.get(random.choice(list(quotes.keys()))))
     else:
-        return "The author doesn't exist"    
-    
+        return "The author doesn't exist"
